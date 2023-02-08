cd538bbedc1e94331a455a7885e0bd332c77e034
SRAVYA-VANI <70704736+SRAVYA-VANI@users.noreply.github.com>
2023-02-08 12:03:16 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] skills = new string[] { "C#", "Java", "SQL" };
+            Instructor instructor = new Instructor("sravya", 4.6f, 4, skills);
+            Console.WriteLine("Is instructor eligible: " + instructor.validateligibility());
+            Console.WriteLine("Does instructor have skill in python: " + instructor.CheckSkill("python"));
+            //if (instructor.validateligibility())
+            //{
+            //    Console.WriteLine("instructor is eligible");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("instructor is not eligible");
+            //}
+            //if (instructor.CheckSkill("java"))
+            //{
+            //    Console.WriteLine("instructor possess the skill");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("instructor doesnot possess the skill");
+            //}
+        }
+        
+    }
+}
